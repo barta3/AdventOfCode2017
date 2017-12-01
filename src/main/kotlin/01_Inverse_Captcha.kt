f657a4002530ae7d94fde4780f8aeca59c6d54d9
barta3 <baertschi.adrian@gmail.com>
2017-12-02 00:11:50 +0100

    01 part 2 (first version)

--- src/main/kotlin/01_Inverse_Captcha.kt
+++ src/main/kotlin/01_Inverse_Captcha.kt
@@ -1,11 +1,10 @@
 class InverseCaptcha {
 
 
-    fun inverseCaptcha(input: String): Int {
-
-
+    fun part1(input: String): Int {
         var res = 0
         val chars = input.toCharArray()
+
         for (i in chars.indices) {
             val firstChar = chars[i]
             val secondIndex = if (i < input.length - 1) i + 1 else 0
@@ -13,6 +12,22 @@ class InverseCaptcha {
                 res += Integer.parseInt(firstChar.toString())
             }
 
+        }
+        return res
+    }
+
+    fun part2(input: String): Int {
+        var res = 0
+        val chars = input.toCharArray()
+        val halfSize = chars.size / 2
+
+        for (i in chars.indices) {
+            val secIdx = if(i + halfSize < chars.size) i + halfSize else i - halfSize
+
+            val secChar = chars[secIdx]
+            if (chars[i] == secChar) {
+                res += Integer.parseInt(chars[i].toString())
+            }
         }
         return res
     }
