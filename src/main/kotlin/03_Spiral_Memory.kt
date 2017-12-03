a3eb0c33e355917f3bc8a5b402e58f7b03d01e68
Adrian Baertschi <adrian.baertschi@zuehlke.com>
2017-12-03 19:32:00 +0100

    03 part 2

--- src/main/kotlin/03_Spiral_Memory.kt
+++ src/main/kotlin/03_Spiral_Memory.kt
@@ -121,14 +121,126 @@ class SpiralMemory {
 
         println("Grid:")
         grid.forEach { row ->
-            row.forEach { print("$it ") }
+            row.forEach { print("$it \t") }
             println()
         }
         println()
     }
 
+    fun part2(input: Int): Int {
+
+        val dimension = (Math.sqrt(input.toDouble()) + 0.5).toInt() + 1
+        println("Input: $input, create $dimension x $dimension grid")
+
+        val grid = arrayListOf<IntArray>()
+        (1..dimension).forEach({ grid.add(IntArray(dimension)) })
+
+
+        val oneX: Int
+        val oneY: Int
+        if (dimension % 2 == 1) {
+            oneX = dimension / 2
+            oneY = dimension / 2
+        } else {
+            oneX = (dimension / 2) - 1
+            oneY = dimension / 2
+        }
+        println("1 is at $oneX $oneY")
+        grid[oneY][oneX] = 1
+
+        printGrid(grid)
+
+
+        // Find coordinate for input
+        var r = 1
+        var u = 1
+        var l = 2
+        var d = 2
+        var resX = oneX
+        var resY = oneY
+
+        var found = false;
+        while (!found) {
+            println("Step $r right")
+            for (i in 1..r) {
+                resX++
+                if (calcAdjacent(resX, resY, grid) > input) {
+                    found = true
+                    break
+                }
+            }
+            r += 2
+            if (found) break
+
+
+            println("Step $u up")
+            for (i in 1..u) {
+                resY--
+                if (calcAdjacent(resX, resY, grid) > input) {
+                    found = true
+                    break
+                }
+            }
+            u += 2
+            if (found) break
+
+            println("Step $l left")
+            for (i in 1..l) {
+                resX--
+                if (calcAdjacent(resX, resY, grid) > input) {
+                    found = true
+                    break
+                }
+            }
+            l += 2
+            if (found) break
+
+            println("Step $d down")
+            for (i in 1..d) {
+                resY++
+                if (calcAdjacent(resX, resY, grid) > input) {
+                    found = true
+                    break
+                }
+            }
+            d += 2
+        }
+
+        printGrid(grid)
+
+        var max = 0
+        grid.forEach { row -> row.forEach { v -> max = Math.max(max, v) } }
+        return max
+    }
+
+    fun calcAdjacent(x: Int, y: Int, grid: ArrayList<IntArray>): Int {
+
+        var res = 0
+        res += getValSafe(grid, y + 1, x - 1)
+        res += getValSafe(grid, y + 1, x)
+        res += getValSafe(grid, y + 1, x + 1)
+        res += getValSafe(grid, y, x - 1)
+        res += getValSafe(grid, y, x + 1)
+        res += getValSafe(grid, y - 1, x - 1)
+        res += getValSafe(grid, y - 1, x)
+        res += getValSafe(grid, y - 1, x + 1)
+
+        grid[y][x] = res
+        return res
+    }
+
+    private fun getValSafe(grid: ArrayList<IntArray>, y: Int, x: Int): Int {
+        try {
+            return grid[y][x]
+        } catch (e: IndexOutOfBoundsException) {
+            return 0
+        }
+    }
+
+
 }
 
 fun main(args: Array<String>) {
     SpiralMemory().calcDistance(8)
+    SpiralMemory().part2(8)
 }
--- src/test/kotlin/SpiralMemoryTest.kt
+++ src/test/kotlin/SpiralMemoryTest.kt
@@ -1,14 +1,21 @@
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class SpiralMemoryTest {
     @Test
     fun calcDistance() {
-        Assert.assertEquals(0, SpiralMemory().calcDistance(1))
-        Assert.assertEquals(3, SpiralMemory().calcDistance(12))
-        Assert.assertEquals(2, SpiralMemory().calcDistance(23))
-        Assert.assertEquals(31, SpiralMemory().calcDistance(1024))
-        Assert.assertEquals(480, SpiralMemory().calcDistance(347991))
+        assertEquals(0, SpiralMemory().calcDistance(1))
+        assertEquals(3, SpiralMemory().calcDistance(12))
+        assertEquals(2, SpiralMemory().calcDistance(23))
+        assertEquals(31, SpiralMemory().calcDistance(1024))
+        assertEquals(480, SpiralMemory().calcDistance(347991))
+    }
+
+    @Test
+    fun testPart2() {
+        assertEquals(10, SpiralMemory().part2(8))
+        assertEquals(54, SpiralMemory().part2(50))
+        assertEquals(349975, SpiralMemory().part2(347991))
     }
 
 }
