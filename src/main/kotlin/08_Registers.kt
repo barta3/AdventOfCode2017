c1ca37f0da56c51610b67cf77ea42f7aa45b0df0
Adrian Baertschi <adrian.baertschi@zuehlke.com>
2017-12-11 20:49:51 +0100

    Day 08 Part 1

--- src/main/kotlin/08_Registers.kt
+++ src/main/kotlin/08_Registers.kt
@@ -34,31 +34,63 @@ data class Instruction(val reg: String,
 
 enum class Operation { INC, DEC }
 
-data class Condition(val reg: String, val comp: Comp, val value: Int)
+data class Condition(val reg: String, val comp: Comp, val value: Int) {
+    fun eval(regs: HashMap<String, Int>): Boolean {
+        if (!regs.containsKey(reg)) {
+            regs.put(reg, 0)
+        }
+
+        val regValue = regs.get(reg)!!
+
+        val contidtion = when (comp) {
+            Comp.GR -> regValue > value
+            Comp.GREQ -> regValue >= value
+            Comp.EQ -> regValue == value
+            Comp.SMEQ -> regValue <= value
+            Comp.SM -> regValue < value
+            Comp.NEQ -> regValue != value
+        }
+
+        return contidtion
+
+    }
+}
 
 enum class Comp(val s: String) {
-    GR(">"), GREQ(">="), EQ("=="), SMEQ("<="), SM("<");
+    GR(">"), GREQ(">="), EQ("=="), SMEQ("<="), SM("<"), NEQ("!=");
 
     companion object {
         fun from(findBy: String): Comp = Comp.values().first { it.s == findBy }
     }
+}
 
+fun run(instructions: ArrayList<Instruction>): HashMap<String, Int> {
+
+    val regs = HashMap<String, Int>()
+
+    instructions.forEach { i ->
+        if (i.condition.eval(regs)) {
+            val amountAbs = if(Operation.DEC == i.operation) (-1) * i.amount else i.amount
+            regs.merge(i.reg, amountAbs, Math::addExact)
+            println("Match $i newVals: $regs")
+        } else {
+            println("No match $i")
+        }
+    }
+
+    return regs
 }
 
 fun main(args: Array<String>) {
-    val instructions = Parser().parse("08_input_example.txt")
+//    val instructions = Parser().parse("08_input_example.txt")
+    val instructions = Parser().parse("08_input_part1.txt")
     instructions.forEach { l -> println(l) }
 
-    val max = run(instructions)
+    val registers = run(instructions)
+    println(registers)
+
+    val maxValue = registers.values.max()
+    println("Max is $maxValue")
 
 }
 
-fun run(instructions: ArrayList<Instruction>): Int {
-
-    val regs = HashMap<String, Int>()
-    instructions.forEach { i ->
-        //regs.computeIfAbsent(i.reg, 0)
-    }
-
-    return 0
-}
