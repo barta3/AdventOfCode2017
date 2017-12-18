f57e1c792537f686be24583f46f5af88df17d805
Adrian Baertschi <adrian.baertschi@zuehlke.com>
2017-12-18 21:15:43 +0100

    Day 11 cleanup

--- src/main/kotlin/11_Hex_Grid.kt
+++ src/main/kotlin/11_Hex_Grid.kt
@@ -2,17 +2,6 @@ import kotlin.math.absoluteValue
 
 class HexGrid {
 
-    val cells = mutableListOf<Cell>()
-    val dim = 10
-
-    fun initGrid() {
-        for (i in -1 * dim..dim) {
-            for (j in -1 * dim..dim)
-                cells.add(Cell(i, j))
-        }
-        println(cells)
-    }
-
     fun goSteps(steps: String) {
         var x = 0
         var y = 0
@@ -45,7 +34,6 @@ data class Cell(val x: Int, val y: Int)
 
 fun main(args: Array<String>) {
     val hexGrid = HexGrid()
-    hexGrid.initGrid()
     hexGrid.goSteps("ne,ne,ne") // 3
     hexGrid.goSteps("ne,ne,sw,sw") // 0
     hexGrid.goSteps("ne,ne,s,s") // 2
